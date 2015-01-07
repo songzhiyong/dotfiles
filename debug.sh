5390deac54949c44f765be640da7a0fed7610f93
songzhiyong <songzhiyong1121@gmail.com>
2015-01-07 11:20:57 +0800

    change script name

--- debug.sh
+++ debug.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+#
+#
+# Simple shell script to debug android project with gradle
+# Usage: debug.sh <mode>
+#          mode b : build debug version
+#          mode u : uninstall application
+#          mode i : install application
+#          mode a : build->uninstall->install
+# @author Jerome Song <songzhiyong1121@gmail.com>
+#
+
 cd ~/dev/android
+mode="$1"
 path=$(awk -F "=" '/root_path/ {print $2}' config.ini)
 pkg_name=$(awk -F "=" '/pkg_name/ {print $2}' config.ini)
 # echo $path
 # echo $pkg_name
+echo "$mode"
 echo "-----build-------"
 cd $path
 ./gradlew :app:assembleDebug
