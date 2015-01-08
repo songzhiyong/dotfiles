614a2c55d32e879ef174bc3fe63dd420ae181955
songzhiyong <songzhiyong1121@gmail.com>
2015-01-08 23:06:41 +0800

    change command option of debug.sh :zzz:

--- debug.sh
+++ debug.sh
@@ -15,14 +15,14 @@ MODE="$1"
 ROOT_PATH=`awk -F "=" '/root_path/ {print $2}' config.ini`
 PKG_NAME=`awk -F "=" '/pkg_name/ {print $2}' config.ini`
 cd $ROOT_PATH
-if [ "$MODE" == "b" ]||[ "$MODE" == "a" ]||[ "$MODE" == "" ];then
+if [[ "$MODE" == *b* ]]||[[ "$MODE" == *a* ]]||[[ "$MODE" == "" ]];then
   ./gradlew :app:assembleDebug
 fi
-if [ "$MODE" == "u" ]||[ "$MODE" == "a" ];then
+if [[ "$MODE" == *u* ]]||[[ "$MODE" == *a* ]];then
   echo "[Uninstall...]"
   adb uninstall $PKG_NAME
 fi
-if [ "$MODE" == "i" ]||[ "$MODE" == "a" ];then
+if [[ "$MODE" == *i* ]]||[[ "$MODE" == *a* ]];then
   echo "[Install...]"
   adb install app/build/outputs/apk/app-debug.apk
 fi
