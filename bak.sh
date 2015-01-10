2f3782e00b36a2b4caba822257c71fa6ad520215
songzhiyong <songzhiyong1121@gmail.com>
2015-01-11 00:23:52 +0800

    change annotation :zzz:

--- bak.sh
+++ bak.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+#================================================================================
+# back up my dotfiles
+# @author Jerome Song <songzhiyong1121@gmail.com>
+#================================================================================
 cp ~/.zshrc ~/dotfiles
 cp ~/.vimrc ~/dotfiles
